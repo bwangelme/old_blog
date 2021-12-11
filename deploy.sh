faf72bf59f05016a9769b6ad6452242ababf7196
bwangelme <bwangel.me@gmail.com>
2021-12-11 23:25:27 +0800

    fix deploy err

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,12 @@ rm -rf public/ && rm -rf .deploy_git/*
 hugo
 cp -r public/* .deploy_git/
 cd .deploy_git
-git reset --hard origin/gh-page
-git pull origin gh-page --rebase
 git add .
 git commit -m "update on $(date '+%Y-%m-%d %H:%M:%S')"
+commit=$(git rev-parse HEAD)
+git reset --hard origin/gh-page
+git pull origin gh-page --rebase
+git cherry-pick $commit
 echo "Start to push"
 git push origin gh-page
+unset commit
