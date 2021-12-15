3d2b5d41dd2437c4114aff17cae806ba24ea9899
bwangelme <bwangel.me@gmail.com>
2021-12-15 15:41:17 +0800

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+echo "Reset Commit"
+cd .deploy_git
+git reset --hard origin/gh-page
+git pull origin gh-page --rebase
+cd ..
+
+echo "Commit changges"
 rm -rf public/ && rm -rf .deploy_git/*
 hugo
 cp -r public/* .deploy_git/
 cd .deploy_git
 git add .
 git commit -m "update on $(date '+%Y-%m-%d %H:%M:%S')"
-commit=$(git rev-parse HEAD)
-git reset --hard origin/gh-page
-git pull origin gh-page --rebase
-git cherry-pick $commit
+
 echo "Start to push"
 git push origin gh-page
 unset commit
